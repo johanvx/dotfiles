05fc3eea69bd2131af99212ddb89a1d28d0b09b8
Johan Xie <johanpoisson@outlook.com>
2026-06-08 22:42:03 +0800

    feat(nvim): add minuet-ai.nvim for autocomplete
    My copilot subscription is inactive. Time to switch to a cheap
    alternative.

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -3,17 +3,38 @@ vim.pack.add({
     src = "https://github.com/saghen/blink.cmp",
     version = vim.version.range("1.*"),
   },
+  { src = "https://github.com/milanglacier/minuet-ai.nvim" },
 })
 
 vim.api.nvim_create_autocmd({ "InsertEnter", "CmdlineEnter" }, {
   group = vim.api.nvim_create_augroup("SetupCompletion", { clear = true }),
   once = true,
   callback = function()
+    local minuet_request_timeout = 3
+    local minuet_timeout_ms = minuet_request_timeout * 1000
+
+    local minuet = require("minuet")
+    minuet.setup({
+      request_timeout = minuet_request_timeout,
+      provider = "openai_fim_compatible",
+      provider_options = {
+        openai_fim_compatible = {
+          api_key = "DEEPSEEK_API_KEY",
+          name = "deepseek",
+          optional = {
+            max_tokens = 4096,
+            top_p = 0.9,
+          },
+        },
+      },
+    })
+
     require("blink.cmp").setup({
       keymap = {
         preset = "default",
         ["<C-space>"] = false,
         ["<C-l>"] = { "show", "show_documentation", "hide_documentation" },
+        ["<C-;>"] = minuet.make_blink_map(),
       },
       appearance = {
         nerd_font_variant = "mono",
@@ -32,8 +53,18 @@ vim.api.nvim_create_autocmd({ "InsertEnter", "CmdlineEnter" }, {
         default = {
           "lsp",
           "path",
-          "snippets",
           "buffer",
+          "snippets",
+          "minuet",
+        },
+        providers = {
+          minuet = {
+            name = "minuet",
+            module = "minuet.blink",
+            async = true,
+            timeout_ms = minuet_timeout_ms,
+            score_offset = 50, -- Gives minuet higher priority among suggestions
+          },
         },
       },
     })
