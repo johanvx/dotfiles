17c4a135706b0af1fb5aab97ba149fc4088ce0df
Johan Xie <johanpoisson@outlook.com>
2026-07-28 16:31:06 +0800

    fix(fish): fix syntax error

--- fish/functions/todo.fish
+++ fish/functions/todo.fish
@@ -1,8 +1,8 @@
 function todo -d "custom todo with ~/todo file"
     set -l todofile $HOME/todo
     test -s $todofile || return
-    type -q rg || { echo "`rg` is not available."; return }
-    type -q sd || { echo "`sd` is not available."; return }
+    type -q rg || begin; echo "`rg` is not available."; return; end
+    type -q sd || begin; echo "`sd` is not available."; return; end
 
     printf '%sTODOs:%s\n' (set_color green) (set_color normal)
     rg -v '^#' $todofile | sd '^(<[^>]*>)(.*)$' (printf '  %s${1}%s${2}' (set_color magenta) (set_color blue))
